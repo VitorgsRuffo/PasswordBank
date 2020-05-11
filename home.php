a35542acd8ded3c4ec9b2b73c523f9cc185384a5
VitorgsRuffo <vitorruffo19cc@gmail.com>
2020-05-11 11:48:03 -0300

    Adding pagination to the home page

--- home.php
+++ home.php
@@ -7,57 +7,86 @@
 		header("Location: index.php");
 	}
 
-	//we'll have to get those rows from accounts table that the logged user owns to show them in this page:
+	//we'll have to get those rows from accounts table that the logged user owns to show them(we are going to show 5 on each page):
 
 		require("config/db_connect.php");
 
 		$userid = $_SESSION['userId'];
 
-		$sql = "SELECT id, service, emailuid, pwd, extra, description FROM accounts WHERE userid = ?";
+		//get the total number of account that the current user has registered:
+			$sql = "SELECT COUNT(*) FROM accounts WHERE userid = ?";
+			$statement = mysqli_stmt_init($connection);
+			if(!mysqli_stmt_prepare($statement, $sql)){
 
-		$statement = mysqli_stmt_init($connection);
+				header("Location: home.php?error=sqlerror");
+				exit();
+			}
+			mysqli_stmt_bind_param($statement, "i", $userid);
+			mysqli_stmt_execute($statement);
+			$result = mysqli_stmt_get_result($statement);
+			$record = mysqli_fetch_assoc($result); //we user this to convert the (only) row returned into an associative array.
+			$totalRecords = $record['COUNT(*)'];
 
-		if(!mysqli_stmt_prepare($statement, $sql)){
+		//getting the current page the user is on: (if no page is specified on the GET request so we are going to be on the first one by default)
+		$page = isset($_GET['page']) && is_numeric($_GET['page']) ? $_GET['page'] : 1;
 
-			header("Location: home.php?error=sqlerror");
-			exit();
-		}
+		//number of account that will be shown on each page:
+		$numOfAccountsOnEachPage = 5;
 
-		mysqli_stmt_bind_param($statement, "i", $userid);
+		//number of pages that will need to displays all records:
+		$totalPages = ceil($totalRecords / $numOfAccountsOnEachPage); 
 
-		mysqli_stmt_execute($statement);
+		//getting the accounts for the current page the user is in:
+			$sql = "SELECT * FROM accounts WHERE userid=? ORDER BY service LIMIT ?,?";
 
-		$rawResult = mysqli_stmt_get_result($statement);
+			if(!mysqli_stmt_prepare($statement, $sql)){
 
-		$resultSet = mysqli_fetch_all($rawResult, MYSQLI_ASSOC); //$resultSet will be an array. Each of its element are associative arrays([columnName] => columnValue) that corresponds to each row returned by the query.
+				header("Location: home.php?error=sqlerror");
+				exit();
+			}
 
-		mysqli_close($connection);
+			$offset = ($page - 1)  * $numOfAccountsOnEachPage;
+			mysqli_stmt_bind_param($statement, "iii", $userid, $offset, $numOfAccountsOnEachPage);
+			mysqli_stmt_execute($statement);
+			$rawResult = mysqli_stmt_get_result($statement);
+			$resultSet = mysqli_fetch_all($rawResult, MYSQLI_ASSOC);  //$resultSet will be an array. Each of its element are associative arrays([column1Name] => column1Value, [column2Name] => column2Value ) that corresponds to each row returned by the query.
+			mysqli_close($connection);
+		
 ?>
 
 	<main>
 		<div>
 			<span><h3>Your passwordbank:</h3></span>
-
-			<div>
-				<ul>	       
-					<?php foreach($resultSet as $row): ?>
-						<div>
-							<div>
-								<li> 
-									<h4> <?php echo htmlspecialchars($row['service']);?> </h4>
-									<p><i>Description:</i> <?php echo htmlspecialchars($row['description']); ?> </p>
-								</li>
-							</div>
-
-							<div>
-								<a href="account-details.php?id=<?php echo htmlspecialchars($row['id']);?>">More</a>
-							</div>
-						</div>
-					<?php endforeach; ?>
-				</ul>	
-			</div>
-
 			<div><a href="add-account.php"><h3>Add new account</h3></a></div>
+			<div>
+				<table>	
+					<tr>
+						<th>Service</th>
+						<th>Description</th>
+						<th>..</th>
+					</tr>       
+					<?php foreach($resultSet as $row): ?>
+						<tr>								
+							<td><h4><?php echo htmlspecialchars($row['service']);?></h4></td>
+							<td><?php echo htmlspecialchars($row['description']);?></td>						
+							<td><a href="account-details.php?id=<?php echo htmlspecialchars($row['id']);?>">More</a></td>						
+						</tr>
+					<?php endforeach; ?>
+				</table>	
+			</div>
+			<div>
+				<?php if($totalPages > 0): ?>
+					<ul>
+						<?php if($page > 1): ?>
+							<li class="previousPage"><a href="home.php?page=<?php echo $page-1;?>">Previous</a></li>
+						<?php endif; ?>
+						<li class="currentPage"><a href="home.php?page=<?php echo $page;?>"><?php echo $page;?></a></li>
+						<?php if($page < $totalPages): ?>
+							<li class="nextPage"><a href="home.php?page=<?php echo $page+1;?>">Next</a></li>
+						<?php endif; ?>
+					</ul>
+				<?php endif; ?>
+			</div>
 		</div>
 	</main>
 
